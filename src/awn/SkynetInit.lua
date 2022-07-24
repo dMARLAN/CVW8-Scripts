082fd2adc41145d066b66865715f8e45cdaf27b2
MARLAN <cpenarsky@shaw.ca>
2022-07-24 15:04:21 -0500

    remove unnecessary line per new version

--- src/awn/SkynetInit.lua
+++ src/awn/SkynetInit.lua
@@ -1,5 +1,4 @@
 redIADS = SkynetIADS:create('IADS')
 redIADS:addEarlyWarningRadarsByPrefix('EWR')
 redIADS:addSAMSitesByPrefix('SAM')
-redIADS:setupSAMSitesAndThenActivate()
 -- redIADS:addRadioMenu() -- Uncomment for debug menu
