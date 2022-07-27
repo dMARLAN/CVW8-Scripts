a633670a600bcd0166ad4354360791c95c22aa80
MARLAN <cpenarsky@shaw.ca>
2022-07-27 00:27:07 -0500

    delete tsta folder

--- src/tsta/SkynetInit.lua
+++
@@ -1,5 +0,0 @@
-redIADS = SkynetIADS:create('IADS')
-redIADS:addEarlyWarningRadarsByPrefix('EWR')
-redIADS:addSAMSitesByPrefix('SAM')
-redIADS:setupSAMSitesAndThenActivate()
--- redIADS:addRadioMenu() -- Uncomment for debug menu
