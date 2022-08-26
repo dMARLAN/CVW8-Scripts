28dd363df0f28075ccdf3a40a839e4fb74a2167c
MARLAN <cpenarsky@shaw.ca>
2022-08-26 03:24:45 -0500

    add ALT vars

--- src/mission_scripts/Supports.lua
+++ src/mission_scripts/Supports.lua
@@ -4,51 +4,47 @@
 
 --- TEXACO 11
 local TO11 = RECOVERYTANKER:New("SOCKEYE Track", "TO11")
-local TO11_alt = 22000
 TO11:SetCallsign(CALLSIGN.Tanker.Texaco, 1)
 TO11:SetTakeoffAir()
-TO11:SetAltitude(TO11_alt)
+TO11:SetAltitude(TANKERS.TO11.ALT)
 TO11:SetRadio(TANKERS.TO11.BTN)
-TO11:SetSpeed(UTILS.KnotsToAltKIAS(TANKERS.SPEED, TO11_alt))
+TO11:SetSpeed(UTILS.KnotsToAltKIAS(TANKERS.SPEED, TANKERS.TO11.ALT))
 TO11:SetTACAN(TANKERS.TO11.TCN_FREQ, TANKERS.TO11.TCN_ID)
 TO11:SetRacetrackDistances(15, 0)
 TO11:__Start(5)
 
 --- TEXACO 21
 local TO21 = RECOVERYTANKER:New("SOCKEYE Track", "TO21")
-local TO21_alt = 24000
 TO21:SetCallsign(CALLSIGN.Tanker.Texaco, 2)
 TO21:SetTakeoffAir()
-TO21:SetAltitude(TO21_alt)
+TO21:SetAltitude(TANKERS.TO21.ALT)
 TO21:SetRadio(TANKERS.TO21.BTN)
-TO21:SetSpeed(UTILS.KnotsToAltKIAS(TANKERS.SPEED, TO21_alt))
+TO21:SetSpeed(UTILS.KnotsToAltKIAS(TANKERS.SPEED, TANKERS.TO21.ALT))
 TO21:SetTACAN(TANKERS.TO21.TCN_FREQ, TANKERS.TO21.TCN_ID)
 TO21:SetRacetrackDistances(15, 0)
-TO21:__Start(10)
+TO21:__Start(30)
 
 --- ARCO 31
 local AO31 = RECOVERYTANKER:New("SNAKEHEAD Track", "AO31")
-local AO31_alt = 21000
 AO31:SetCallsign(CALLSIGN.Tanker.Arco, 3)
 AO31:SetTakeoffAir()
-AO31:SetAltitude(AO31_alt)
+AO31:SetAltitude(TANKERS.AO31.ALT)
 AO31:SetRadio(TANKERS.AO31.BTN)
-AO31:SetSpeed(UTILS.KnotsToAltKIAS(TANKERS.SPEED, AO31_alt))
+AO31:SetSpeed(UTILS.KnotsToAltKIAS(TANKERS.SPEED, TANKERS.AO31.ALT))
 AO31:SetTACAN(TANKERS.AO31.TCN_FREQ, TANKERS.AO31.TCN_ID)
 AO31:SetRacetrackDistances(15, 0)
-AO31:__Start(10)
+AO31:__Start(5)
 
 --- ARCO 41
 local AO41 = RECOVERYTANKER:New("SNAKEHEAD Track", "AO41")
-local AO41_alt = 23000
 AO41:SetCallsign(CALLSIGN.Tanker.Arco, 4)
 AO41:SetTakeoffAir()
-AO41:SetAltitude(AO41_alt)
+AO41:SetAltitude(TANKERS.AO41.ALT)
 AO41:SetRadio(TANKERS.AO41.BTN)
-AO41:SetSpeed(UTILS.KnotsToAltKIAS(TANKERS.SPEED, AO41_alt))
+AO41:SetSpeed(UTILS.KnotsToAltKIAS(TANKERS.SPEED, TANKERS.AO41.ALT))
 AO41:SetTACAN(TANKERS.AO41.TCN_FREQ, TANKERS.AO41.TCN_ID)
 AO41:SetRacetrackDistances(15, 0)
-AO41:__Start(10)
+AO41:__Start(30)
 
 --------------------
 --- AWACS Track ----
@@ -76,7 +72,7 @@ OFS1:SetAltitude(OFS1_alt)
 OFS1:SetRadio(RADIOS.BTN_10)
 OFS1:SetSpeed(UTILS.KnotsToAltKIAS(AWACS.SPEED, OFS1_alt))
 OFS1:SetRacetrackDistances(30, 0)
-OFS1:__Start(15)
+OFS1:__Start(60)
 
 --- WIZARD (Red AI AWACS)
 local ORS1 = RECOVERYTANKER:New("Red AWACS Track", "ORS1")
@@ -99,4 +95,4 @@ OWZ1:SetAltitude(OWZ1_alt)
 OWZ1:SetRadio(RADIOS.BTN_12)
 OWZ1:SetSpeed(UTILS.KnotsToAltKIAS(AWACS.SPEED, OWZ1_alt))
 OWZ1:SetRacetrackDistances(30, 0)
-OWZ1:__Start(15)
+OWZ1:__Start(60)
