30e871fa0ea8189348b3c605eaad409f41a89070
MARLAN <cpenarsky@shaw.ca>
2022-08-27 03:32:21 -0500

    rename file

--- src/CVW8ScriptsLoader.lua
+++ src/CVW8ScriptsLoader.lua
@@ -3,15 +3,13 @@ require "lfs"
 local LIBRARIES_FOLDER = "libraries"
 local CONSTANTS_FOLDER = "constants"
 local MISSION_SCRIPTS_FOLDER = "mission_scripts"
-local MODULE_NAME = "CVW8-Scripts"
-local THIS_FILE = MODULE_NAME .. ".ScriptLoader"
+CVW8Scripts.SCRIPTS_PATH = lfs.writedir() .. "Missions\\" .. CVW8Scripts.MISSION_FOLDER
 
 local function loadAllLua(folder)
-    local dir = DCSWeather.SCRIPTS_PATH .. "\\" .. folder
+    local dir = CVW8Scripts.SCRIPTS_PATH .. "\\" .. folder
     for file in lfs.dir(dir) do
         if string.find(file, ".lua$") then
             dofile(dir .. "\\" .. file)
-            DCSWeather.Logger.Info(THIS_FILE, "Loaded: " .. folder .. "\\" .. file)
         end
     end
 end
