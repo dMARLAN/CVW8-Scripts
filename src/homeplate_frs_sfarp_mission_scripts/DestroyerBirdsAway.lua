3a471fd2faf8d45ac9019584c298bac7b3f82085
MARLAN <cpenarsky@shaw.ca>
2022-11-08 04:58:06 -0600

    updated

--- src/homeplate_frs_sfarp_mission_scripts/DestroyerBirdsAway.lua
+++ src/homeplate_frs_sfarp_mission_scripts/DestroyerBirdsAway.lua
@@ -1,13 +1,14 @@
 local shipNames = {}
 local shotHandler = {}
-local addShip, isASpecifiedShip, getBullseye, getImpact, buildMessage, getBearingFromTwoPoints, getDistanceFromTwoPoints, getMagneticDeclination, integerToEnglish, bearingToSingleDigits
-local freqs = { "255", "262", "259", "268" }
+local addShip
+local freqs = "255,262,259,268"
+local freqMod = "AM,AM,AM,AM"
 
 function addShip(shipGroupName, shipCallsign)
     shipNames[shipGroupName] = shipCallsign
 end
 
-function isASpecifiedShip(shipToCheck)
+local function isASpecifiedShip(shipToCheck)
     if (not shipToCheck) then
         return false
     end
@@ -20,66 +21,29 @@ function isASpecifiedShip(shipToCheck)
     return false
 end
 
-function getBullseye(target)
-    local bullsLO = coalition.getMainRefPoint(coalition.side.BLUE)
-    local distanceNm = math.floor(getDistanceFromTwoPoints(bullsLO, target:getPoint()) / 1852)
-
-    if (distanceNm < 5) then
-        return ", AT BULLSEYE "
-    end
-
-    local distanceNmEnglish = integerToEnglish(distanceNm)
-    local bearing = math.floor(getBearingFromTwoPoints(bullsLO, target:getPoint()) - getMagneticDeclination() * 2)
-    local bearingString = bearingToSingleDigits(bearing)
-
-    return "BULLSEYE, " .. bearingString .. " , " .. distanceNmEnglish .. ", "
+local function getDistanceFromTwoPoints(p1, p2)
+    return math.sqrt(math.pow(p2.x - p1.x, 2) + math.pow(p2.z - p1.z, 2))
 end
 
-function bearingToSingleDigits(bearing)
+local function bearingToSingleDigits(bearing)
     local bearingString = ""
+    if ( bearing < 100) then
+        bearingString = bearingString .. "ZERO-"
+    end
+    if ( bearing < 10) then
+        bearingString = bearingString .. "ZERO-ZERO-"
+    end
     for i = 1, string.len(bearing) do
-        bearingString = bearingString .. string.sub(bearing, i, i) .. " "
+        if (string.sub(bearing, i, i) == "0") then
+            bearingString = bearingString .. "ZERO-"
+        else
+            bearingString = bearingString .. string.sub(bearing, i, i) .. "-"
+        end
     end
     return bearingString
 end
 
-local belowTen = {"ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE", "TEN"}
-local belowTwenty = {"ELEVEN", "TWELVE", "THIRTEEN", "FOURTEEN", "FIFTEEN", "SIXTEEN", "SEVENTEEN", "EIGHTEEN", "NINETEEN"};
-local belowHundred = {"", "TWENTY", "THIRTY", "FORTY", "FIFTY", "SIXTY", "SEVENTY", "EIGHTY", "NINETY"};
-
-local function integerToEnglishHelper(num)
-    if (num == 0) then
-        return " "
-    end
-    if (num < 11) then
-        return belowTen[num]
-    end
-
-    if (num < 20) then
-        return belowTwenty[num - 10]
-    end
-
-    if (num < 100) then
-        return belowHundred[math.floor(num/10)] .. " " .. integerToEnglishHelper(num % 10)
-    else
-        local middleWord = " HUNDRED AND "
-        if (num % 100 == 0) then
-            middleWord = " HUNDRED "
-        end
-        return integerToEnglishHelper(math.floor(num / 100)) .. middleWord .. integerToEnglishHelper(num % 100)
-    end
-end
-
-function integerToEnglish(distance)
-    return integerToEnglishHelper(distance)
-end
-
-
-function getDistanceFromTwoPoints(p1, p2)
-    return math.sqrt(math.pow(p2.x - p1.x, 2) + math.pow(p2.z - p1.z, 2))
-end
-
-function getBearingFromTwoPoints(p1, p2)
+local function getBearingFromTwoPoints(p1, p2)
     local p1Lat, p1Lon, _ = coord.LOtoLL(p1)
     local p2Lat, p2Lon, _ = coord.LOtoLL(p2)
     local lat1 = math.rad(p1Lat)
@@ -93,7 +57,7 @@ function getBearingFromTwoPoints(p1, p2)
     return (math.deg(math.atan2(y, x)) + 360) % 360
 end
 
-function getMagneticDeclination()
+local function getMagneticDeclination()
     local theatre = env.mission.theatre
     if theatre == "Caucasus" then
         return 6
@@ -116,15 +80,79 @@ function getMagneticDeclination()
     return 0
 end
 
-
-function getImpact(target)
+local function getBullseye(target)
     local bullsLO = coalition.getMainRefPoint(coalition.side.BLUE)
     local distanceNm = math.floor(getDistanceFromTwoPoints(bullsLO, target:getPoint()) / 1852)
 
-    return " IMPACT, ONE PLUS FIFTEEN"
+    if (distanceNm < 5) then
+        return ", AT BULLSEYE "
+    end
+
+    local bearing = math.floor(getBearingFromTwoPoints(bullsLO, target:getPoint()) - getMagneticDeclination() * 2)
+    local bearingString = bearingToSingleDigits(bearing)
+
+    return "BULLSEYE, " .. bearingString .. " , " .. distanceNm .. ", "
 end
 
-function buildMessage(shipCallsign, bullseye, impact)
+local function dot(b, c)
+    return b.x * c.x + b.y * c.y
+end
+
+local function magnitude(v)
+    return math.sqrt(v.x * v.x + v.y * v.y)
+end
+
+local function angleBetween(b, c)
+    return math.acos(dot(b, c) / (magnitude(b) * magnitude(c)))
+end
+
+local function vecMinus(a, b)
+    return { x = a.x - b.x, y = a.z - b.z }
+end
+
+local function timeToImpact(targetPos, targetVel, interceptorPos, interceptorSpeed)
+    targetVel = { x = targetVel.x, y = targetVel.z }
+    local k = magnitude(targetVel) / interceptorSpeed
+    local distanceToTarget = magnitude(vecMinus(interceptorPos, targetPos))
+
+    local bHat = targetVel
+    local cHat = vecMinus(interceptorPos, targetPos)
+
+    local CAB = angleBetween(bHat, cHat)
+    local ABC = math.asin(k * math.sin(CAB))
+    local ACB = math.pi - CAB - ABC
+
+    local j = distanceToTarget / math.sin(ACB)
+    local b = j * math.sin(ABC)
+
+    local timeToGo = b / magnitude(targetVel)
+    -- local impactPoint = targetPos + (targetVel * timeToImpact)
+
+    return timeToGo + 10
+end
+
+local function timeToEnglish(time)
+    time = math.floor(time)
+    if (time < 60) then
+        return "ZERO PLUS " .. time
+    end
+    if (time > 60 and time < 120) then
+        return "ONE PLUS " .. (time - 60)
+    end
+    if (time > 120 and time < 180) then
+        return "TWO PLUS " .. (time - 120)
+    end
+    if (time > 180 and time < 240) then
+        return "THREE PLUS " .. (time - 180)
+    end
+    return time
+end
+
+local function getImpact(interceptor, target)
+    return " IMPACT, " .. timeToEnglish(timeToImpact(target:getPoint(), target:getVelocity(), interceptor:getPoint(), 1000))
+end
+
+local function buildMessage(shipCallsign, bullseye, impact)
     local msg = {}
     msg[#msg + 1] = shipCallsign
     msg[#msg + 1] = ", BIRDS AWAY, TARGETED, " .. bullseye .. impact
@@ -138,9 +166,8 @@ function shotHandler:onEvent(event)
         local bullseye = getBullseye(target)
         local impact = getImpact(event.initiator, target)
         local message = buildMessage(shipCallsign, bullseye, impact)
-        for _, freq in pairs(freqs) do
-            STTS.TextToSpeech(message, freq, "AM", "1.0", shipCallsign, 2, null, 2, "male", "en-GB")
-        end
+        trigger.action.outText(STTS.getSpeechTime(message, 1, true),10)
+        STTS.TextToSpeech(message, freqs, freqMod, "1.0", shipCallsign, coalition.side.BLUE, event.initiator:getPoint(), 1, "male", "en-US", "en-US-Standard-J", true)
     end
 end
 
@@ -150,5 +177,3 @@ local function main()
     world.addEventHandler(shotHandler)
 end
 main()
-
-
